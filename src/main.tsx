96f314af9d6438f2aac551289c7fe5e86386198d
johnnymaconny <johnnymaconny@gmail.com>
2025-03-02 21:51:30 -0800

    fix: update App component file casing for case-sensitive environments

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from './app'
+import App from './App'
 import './index.css'
 import { AuthProvider } from './contexts/AuthContext'
 import { AdminProvider } from './contexts/AdminContext'
