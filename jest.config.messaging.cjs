21087dbc84b7993e85031edd16e978102c1665ca
JOHNNYMACONNY <58898456+JOHNNYMACONNY@users.noreply.github.com>
2025-09-27 00:01:19 -0700

    CI: isolate emulator tests with dedicated Jest config (node env, no jsdom setup)

---
+++ jest.config.messaging.cjs
@@ -0,0 +1,40 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  // Do NOT load jsdom-specific setup to avoid referencing window in Node env
+  setupFilesAfterEnv: [],
+  moduleNameMapper: {
+    '^@firebase/rules-unit-testing$': '<rootDir>/src/types/firebase-test.cjs',
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        tsconfig: {
+          jsx: 'react-jsx',
+          esModuleInterop: true,
+          module: 'esnext',
+          moduleResolution: 'node',
+          allowJs: true,
+          strict: true,
+          isolatedModules: true,
+          skipLibCheck: true,
+          types: ['jest', 'node'],
+        },
+        babelConfig: {
+          plugins: [
+            '@babel/plugin-syntax-import-meta',
+            ['@babel/plugin-transform-modules-commonjs', { allowTopLevelThis: true }],
+          ],
+        },
+      },
+    ],
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  transformIgnorePatterns: ['node_modules/(?!(firebase|@firebase)/)'],
+  verbose: true,
+};
